3cd8baf345eabc5f97f5a98102b7d341994ecac6
Sabagelashh <147238067+Sabagelashh@users.noreply.github.com>
2024-03-25 11:51:16 +0400

    Create R11.java

---
+++ chemicalop/R11.java
@@ -0,0 +1,9 @@
+package quizzes.q2.saba_gelashvili_2.chemicalop;
+
+import quizzes.q2.saba_gelashvili_2.motorcycle.R12;
+
+public class R11 {
+    public static void main(String[] args) {
+        System.out.println(R12.MESSAGE);
+    }
+}
