2174280395b5dc77677750cec1c69716d83fa6a5
Sabagelashh <147238067+Sabagelashh@users.noreply.github.com>
2024-03-25 11:54:01 +0400

    Create R12.java

---
+++ motorcycle/R12.java
@@ -0,0 +1,6 @@
+// Ensure this package name matches the package of A12.java
+package quizes.q2.saba_gelashvili_2.motorcycle;
+
+public class R12 {
+    public static final String MESSAGE = "Hello from the R12";
+}
